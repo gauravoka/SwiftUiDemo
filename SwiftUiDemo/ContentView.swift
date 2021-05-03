833ff60561bc67213a78732437cd846495f25d7b
Gaurav Oka <gauravoka1792@gmail.com>
2021-05-03 22:28:05 +0530

    Initial Commit

---
+++ SwiftUiDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiDemo
+//
+//  Created by Gaurav Oka on 03/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
